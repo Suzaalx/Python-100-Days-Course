91809f857d91794b762164ed298cbcf15831e6bf
SUZAALX <122628569+Suzaalx@users.noreply.github.com>
2023-03-15 22:59:46 +0545

    Done with day 2
    Still strong.
    Learned quite a few new things today.
    Kinda excited for Day 3.

---
+++ Day-2/Day2.py
@@ -0,0 +1,8 @@
+print("Welcome to the tip calculator! ")
+bill=input("What was the total bill? " ) 
+tip=input("How much tip would you like to give? 10, 12, or 15? " )  
+person = input("How many people to split the bill? " ) 
+tip_decimal = int(tip)/100 +1
+pay= (int(bill)/int(person)) * tip_decimal
+final_pay = '{0:.2f}'.format(pay)
+print(f"Each person should pay: {final_pay}")
