9c912f97e755d38c6768e1a9b04327f8c24c2061
SUZAALX <122628569+Suzaalx@users.noreply.github.com>
2023-03-21 22:14:51 +0545

    Delete Day-02 directory

--- Day-02/Day2.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculator! ")
-bill=input("What was the total bill? " ) 
-tip=input("How much tip would you like to give? 10, 12, or 15? " )  
-person = input("How many people to split the bill? " ) 
-tip_decimal = int(tip)/100 +1
-pay= (int(bill)/int(person)) * tip_decimal
-final_pay = '{0:.2f}'.format(pay)
-print(f"Each person will pay: {final_pay}")
