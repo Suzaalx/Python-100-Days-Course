9f20598594da7a75fb2de9d0921da337d85115ed
Suzaalx <sujalxetry00@gmail.com>
2023-03-26 22:35:49 +0545

    circle with turtle

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,5 +1,12 @@
 from turtle import Turtle, Screen
 
-the_turte =Turtle()
+the_turtle =Turtle()
+the_turtle.shape("circle")
+for i in range(4):
+    the_turtle.forward(100)
+    the_turtle.left(90)
+
+
 screen=Screen()
 screen.exitonclick()
+
