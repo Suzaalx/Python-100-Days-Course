c0dfb37113f734327f1c16eb857bd41cc58203da
Suzaalx <sujalxetry00@gmail.com>
2023-03-29 15:28:43 +0545

    part-1 done

--- Day-20/snake.py
+++ Day-20/snake.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-X=20
+X=0
 MOVE_DISTANCE=20
 # segments=[]
 # for i in range(3):
@@ -23,20 +23,21 @@ class Snake:
          self.segments=[]
          self.create_snake()
     def create_snake(self):
-        for i in range(3):
-            global X
+        global X
+        for i in range(4):
+            
             snake=Turtle(shape="square")
             snake.color("white")
             snake.penup()
-            snake.goto(x=-X,y=0)
-            X+=20
+            snake.goto(x=X,y=0)
+            X=X-20
             self.segments.append(snake)
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
             new_x=self.segments[seg_num-1].xcor()
             new_y=self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x,new_y)
-            self.segments[0].fd(MOVE_DISTANCE)
+        self.segments[0].fd(MOVE_DISTANCE)
     
     def up(self):
         self.segments[0].setheading(90) 
