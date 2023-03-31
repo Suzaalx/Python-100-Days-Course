852d51e105766d75a77d6f9a225943cd3c2a676b
Suzaalx <sujalxetry00@gmail.com>
2023-03-31 22:40:49 +0545

    initial commit

---
+++ Day-22/player.py
@@ -0,0 +1,18 @@
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+from turtle import Turtle
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()  
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+    def move(self):
+        
+        self.fd(MOVE_DISTANCE)
+        
+        
