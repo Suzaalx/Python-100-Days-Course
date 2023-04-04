20997839eab351f903494e8a33b6bed865e4a862
Suzaalx <sujalxetry00@gmail.com>
2023-04-04 21:05:25 +0545

    mile converter

--- Day-27/main.py
+++ Day-27/main.py
@@ -1,19 +1,38 @@
 from tkinter import *
 
 window=Tk()
-window.title("Day-27")
-window.minsize(500,300)
+window.title("Mile to Km converter")
+window.config(padx=25,pady=20)
 
-lable=Label(text="Hello how are you?" ,font=('arial',18,))
-lable.grid(column=0,row=0)
-def clicked():
-    lable.config(text=input.get())
 
-input=Entry(width=30)
-input.grid(column=1,row=1)
+def convert():
+    miles=input.get()
+    km_value= round(float(miles) * 1.609)
+    num.config(text=km_value)
 
-button = Button(text="Click me!",command=clicked)
-button.grid(row=2,column=2)
+input=Entry(width=7)
+input.grid(column=1,row=0)
+
+mile=Label(text="Miles")
+mile.grid(column=2,row=0)
+
+is_equal=Label(text="is equal to" )
+is_equal.grid(column=0,row=1)
+
+num=Label(text="0" ,font=('arial',15))
+num.grid(column=1,row=1)
+
+km=Label(text="km" )
+km.grid(column=2,row=1)
+
+
+button = Button(text="Calculate!",command=convert)
+button.grid(column=1,row=2)
+
+
+
+
+
 
 window.mainloop()
 
