8685d1ba5d07b4eecced726ad2f40ad3601dd1d5
Suzaalx <sujalxetry00@gmail.com>
2023-04-05 22:18:54 +0545

    pomodoro

--- Day-28/main.py
+++ Day-28/main.py
@@ -10,22 +10,60 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps=0
+counter=""
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    global reps
+    window.after_cancel(counter)
+    reps=0
+    round.config(text="")
+    title.config(text="Timer",fg=RED)
+    canvas.itemconfig(timer_text, text="00:00")
+    start.config(state="normal")
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5*60)
-# -------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    global reps
+    reps+=1
+    work_sec= WORK_MIN*60
+    break_sec=SHORT_BREAK_MIN*60
+    long_break=LONG_BREAK_MIN*60
+    window.attributes("-topmost",1)
+    if reps%2!=0:
+        count_down(work_sec)
+        title.config(text="Work",fg=GREEN)
+    elif reps%2==0:
+        count_down(break_sec)
+        title.config(text="Short break", fg=RED)
+    elif reps%8==0:
+        count_down(long_break)
+        title.config(text="Long break",fg=RED)
+    
+    start.config(state="disabled")
+    window.attributes('-topmost', 0) 
+    
+    
+    
+# -----------------------(--- COUNTDOWN MECHANISM ------------------------------- # 
 
 def count_down(count):
-    
+    global counter
     count_min=math.floor(count/60)
     count_sec=count%60
+    if count_sec<10:
+        count_sec=f"0{str(count_sec)}"
+    
     canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     print(count)
     if count>0:
-        window.after(1000,count_down,count-1)
+        counter=window.after(1000,count_down,count-1)
+    else:
+        start_timer()
+        mark=""
+        session=math.floor(reps/2)
+        for i in range(session):
+            mark+="✓"
+        round.config(text=mark)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -48,10 +86,10 @@ title.grid(column=1,row=0)
 start=Button(text="Start",bg=PINK , fg=YELLOW, font=(FONT_NAME, 15, "bold"),command=start_timer)
 start.grid(column=0,row=2)
 
-round=Label(text="✓",bg=PINK , fg=YELLOW, font=(FONT_NAME, 20, "italic"))
+round=Label(text="",bg=PINK , fg=YELLOW, font=(FONT_NAME, 20, "italic"))
 round.grid(column=1,row=3)
 
-reset=Button(text="Reset",bg=PINK , fg=YELLOW, font=(FONT_NAME, 15, "bold"))
+reset=Button(text="Reset",bg=PINK , fg=YELLOW, font=(FONT_NAME, 15, "bold"),command=reset_timer)
 reset.grid(column=2,row=2)
 
 window.mainloop()
