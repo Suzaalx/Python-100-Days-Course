910bb7f11b7c65846d4a96bf11a1208393f24595
Suzaalx <sujalxetry00@gmail.com>
2023-04-05 23:11:10 +0545

    grid fixed

--- Day-29/main.py
+++ Day-29/main.py
@@ -14,7 +14,7 @@ canvas=Canvas(width=200,height=200)
 img=PhotoImage(file="Day-29/logo.png")
 canvas.create_image(100,100,image=img)
 canvas.grid(column=1,row=0)
-
+#label grids
 web=Label(text="Website:")
 web.grid(column=0,row=1)
 
@@ -24,23 +24,24 @@ email.grid(column=0,row=2)
 password=Label(text="Password:")
 password.grid(column=0,row=3)
 
+#Entry grids. 
+#sticky="EW" makes the widget stick to the edges so that the layout looks better.
 web_name=Entry(width=35)
-web_name.grid(column=1,row=1,columnspan=2)
+web_name.grid(column=1,row=1,columnspan=2, sticky="EW")
 
 user_name=Entry(width=35)
-user_name.grid(column=1,row=2,columnspan=2)
+user_name.grid(column=1,row=2,columnspan=2, sticky="EW")
 
 pass_name=Entry(width=21)
-pass_name.grid(column=1,row=3)
+pass_name.grid(column=1,row=3, sticky="EW")
 
+
+#button grids
 generate_button= Button(text="Generate Password")
-generate_button.grid(column=2,row=3)
-
-
-
+generate_button.grid(column=2,row=3, sticky="EW")
 
 button=Button(text="Add",width=36)
-button.grid(column=1,row=4,columnspan=2)
+button.grid(column=1,row=4,columnspan=2, sticky="EW")
 
 
 
