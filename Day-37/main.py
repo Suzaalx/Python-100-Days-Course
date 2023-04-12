3358ed35946084a9fd5d7adf28f2a215c8d6f0db
Suzaalx <sujalxetry00@gmail.com>
2023-04-12 21:33:50 +0545

    created graph using https header

--- Day-37/main.py
+++ Day-37/main.py
@@ -13,7 +13,19 @@ user_params={
     
 }
 
-# response=requests.post(url=pixela_endpoint,json= user_params)
-# print(response.text)
 
 graph_endpoint= f"{pixela_endpoint}/{username}/graphs"
+
+graph_config={
+    "id": "graph1",
+    "name": "running",
+    "unit": "kilogram",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers={
+    "X-USER-TOKEN": token
+}
+response=requests.post(url=graph_endpoint,json= graph_config, headers= headers)
+print(response.text)
