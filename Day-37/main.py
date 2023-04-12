5e0f09f709eff2a0b3358d509bc277b46b32920d
Suzaalx <sujalxetry00@gmail.com>
2023-04-12 22:08:16 +0545

    added datetime

--- Day-37/main.py
+++ Day-37/main.py
@@ -1,10 +1,11 @@
 import requests
-
+from datetime import datetime
 username= "boksi00"
 token= "8888888888"
-
+graph_id= "graph1"
 pixela_endpoint= "https://pixe.la/v1/users"
-
+today= datetime.now()
+print(today)
 user_params={
     "token": token,
     "username": username,
@@ -27,5 +28,19 @@ graph_config={
 headers={
     "X-USER-TOKEN": token
 }
-response=requests.post(url=graph_endpoint,json= graph_config, headers= headers)
+# response=requests.post(url=graph_endpoint,json= graph_config, headers= headers)
+# print(response.text)
+
+pixel_endpoint=f'{pixela_endpoint}/{username}/graphs/{graph_id}'
+
+
+pixel_config={
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "10.5",
+    
+}
+
+
+
+response= requests.post(url=pixel_endpoint,json=pixel_config,headers=headers)
 print(response.text)
