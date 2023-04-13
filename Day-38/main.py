ca549816267fa77336193fde3ffbf297a5375a36
Suzaalx <sujalxetry00@gmail.com>
2023-04-13 22:40:19 +0545

    added apis to local env

--- Day-38/main.py
+++ Day-38/main.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 import requests
-APP_ID= "5d8d9fe8"
+import os
+from dotenv import load_dotenv
+load_dotenv("D:/SAT/ss/Documents/docs/EnvironmentVariable/.env")
 
-APP_KEY= "c289ca348b0d2a2aa4238794f834394f"
+#nutritionix apis
+APP_ID= "5d8d9fe8"
+APP_KEY= os.getenv("nutrionix_key")
 
 
 exercise_endpoint= "https://trackapi.nutritionix.com/v2/natural/exercise"
@@ -17,16 +21,18 @@ exercise_params={
 
 headers={
     "x-app-id": APP_ID,
-    "x-app-key": APP_KEY
+    "x-app-key": f"{APP_KEY}",
 }
 
 response= requests.post(url= exercise_endpoint, json= exercise_params,headers= headers).json()
-print(response)
+# print(response)
 
 
-#sheety part
+# sheety part
 date= datetime.today().strftime("%d/%m/%Y")
 time= datetime.now().strftime("%H:%M:%S")
+sheety_auth= os.getenv("sheety_auth")
+sheety_header={"Authorization": f"{sheety_auth}"}
 
 sheety_endpoint= "https://api.sheety.co/82e8dd17b9fcb26b107ec1682b2a61e5/myWorkouts/workouts"
 for n in response["exercises"]:
@@ -39,7 +45,7 @@ for n in response["exercises"]:
             "calories": n["nf_calories"],
         }
     }
-    document=requests.post(url=sheety_endpoint,json=sheety_config)
-    print(document.text)
+    document=requests.post(url=sheety_endpoint,json=sheety_config,headers=sheety_header)
     print(document.status_code)
+    print(document.text)
 
