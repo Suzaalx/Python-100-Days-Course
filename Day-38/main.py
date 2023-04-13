a79aaf6403ac77f990c61e4052c4cf8f0cb7c934
Suzaalx <sujalxetry00@gmail.com>
2023-04-13 22:24:27 +0545

    nutritionx and sheety api

--- Day-38/main.py
+++ Day-38/main.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 import requests
 APP_ID= "5d8d9fe8"
 
@@ -8,7 +8,11 @@ APP_KEY= "c289ca348b0d2a2aa4238794f834394f"
 exercise_endpoint= "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 exercise_params={
-    "query": input("What exercise did you do?: ")
+    "query": input("What exercise did you do?: "),
+    "gender": "male",
+    "weight_kg": 65,
+    "height_cm": 180,
+    "age": 19
 }
 
 headers={
@@ -16,5 +20,26 @@ headers={
     "x-app-key": APP_KEY
 }
 
-response= requests.post(url= exercise_endpoint, json= exercise_params,headers= headers)
-print(response.json())
+response= requests.post(url= exercise_endpoint, json= exercise_params,headers= headers).json()
+print(response)
+
+
+#sheety part
+date= datetime.today().strftime("%d/%m/%Y")
+time= datetime.now().strftime("%H:%M:%S")
+
+sheety_endpoint= "https://api.sheety.co/82e8dd17b9fcb26b107ec1682b2a61e5/myWorkouts/workouts"
+for n in response["exercises"]:
+    sheety_config={
+        "workout":{
+            "date": date,
+            "time": time,
+            "exercise": n["name"].title(),
+            "duration": n["duration_min"],
+            "calories": n["nf_calories"],
+        }
+    }
+    document=requests.post(url=sheety_endpoint,json=sheety_config)
+    print(document.text)
+    print(document.status_code)
+
