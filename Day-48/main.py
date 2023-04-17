5c61b7b61338bdd47fc9f76355c64d05c2f2ce6e
Suzaalx <sujalxetry00@gmail.com>
2023-04-17 09:58:35 +0545

    final commit

--- Day-48/main.py
+++ Day-48/main.py
@@ -1,17 +1,28 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+import time
 
+URL = "http://orteil.dashnet.org/experiments/cookie/"
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(URL)
 
-driver = webdriver.Chrome()
-driver.get("http://orteil.dashnet.org/experiments/cookie/")
+cookie = driver.find_element(By.ID, "cookie")
+money = driver.find_element(By.ID, "money")
 
-cookie=driver.find_element(By.ID,"cookie")
-cookie_count=driver.find_element(By.ID,"money")
-print(cookie_count.text)
-actions = ActionChains(driver)
-actions.click(cookie)
-for i in range(5000):
-    
-    actions.perform()
+timeout = time.time() + 5
+play_time = time.time() + 60 * 5
+while time.time() < play_time:
+    cookie.click()
+
+    if time.time() > timeout:
+        items = driver.find_elements(By.CSS_SELECTOR, "#store > div:not(.grayed)")
+
+        if len(items) > 0:
+            items[-1].click()
+
+        timeout = time.time() + 5
+
+print("Your final score:", driver.find_element(By.ID, "cps").text)
+driver.save_screenshot("result.png")
